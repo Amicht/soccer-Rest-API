401f42e7b8fc9184b38263926450f5807ff042c8
Amicht <vicolicht@gmail.com>
2022-06-21 03:51:50 +0300

    player ctrl added/changed

--- src/server/4-controllers-layer/players-controller.js
+++ src/server/4-controllers-layer/players-controller.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const logic = require('../3-logic/logic-layer');
 const dal = require('../2-data-access-layer/dal');
 const ErrorModel = require('../models/error-model');
 const playersCtrl = express.Router();
@@ -13,32 +12,60 @@ playersCtrl.get('/',async (req,res,next)=> {
 
 playersCtrl.get("/filter",async (req,res,next)=> {
     try{
-        const players = await dal.getAllPlayersAsync((err,players)=> logic.getPlayersFilter(players,req.query)
-        .then(players => res.json(players)));
-        //if(!players) return next(new ErrorModel(404,`No results`));
-        //res.json(players);
+        const minGoals = req.query.minGoals || 0;
+        const minassists = req.query.minAssists || 0;
+        dal.getPlayersByStats(minGoals,minassists,(err,players)=> res.json(players));
     }
     catch(err){next(err)}
 })
 
-playersCtrl.get('/:id([0-9]+)',async (req,res,next)=>{
+playersCtrl.get('/team/:team',async (req,res,next)=>{
     try{
-        dal.getPlayerByIdAsync(+req.params.id, (err, player)=> res.json(player));
+        if(!req.params.team) return next(new ErrorModel(404, 'param dont exists'));
+        dal.getPlayersByTeam(req.params.team, (err, players)=> res.json(players));
+    }
+    catch(err){next(err)}
+});
+playersCtrl.get('/position/:pos',async (req,res,next)=>{
+    try{
+        if(!req.params.pos) return next(new ErrorModel(404, 'param dont exists'));
+        dal.getPlayersByPos(req.params.pos, (err, players)=> res.json(players));
     }
     catch(err){next(err)}
 });
 
+
 // currently only 'scorer' / 'assists'
 playersCtrl.get('/top/:topParam',async (req,res,next)=> {
-    dal.getAllPlayersAsync( (err, players)=> logic.getTopPlayer(players,req.params.topParam)
-    .then(players => res.json(players)).catch(err => next(err)));
+    try{
+        if(req.params.topParam==='assists'){
+            dal.getTopAssist((err,player) => res.json(player[0]));
+        }
+        else if(req.params.topParam==='scorrer'){
+            dal.getTopScorrer((err,player) => res.json(player[0]));
+        }
+        else{
+            return next(new ErrorModel(404, 'param doesnt exists'))
+        }
+    }catch(err){ next(err) }
+});
+
+playersCtrl.get('/:id([0-9]+)',async (req,res,next)=>{
+    try{
+        dal.getPlayerByIdAsync(req.params.id, (err, player) => {
+            if(!player) return next(new ErrorModel(404, `Player with id ${req.params.id} dont exist`))
+            res.json(player[0]);
+        });
+    }
+    catch(err){next(err)}
 });
 
 playersCtrl.get('/:name',async (req,res,next)=> {
     try{
-        dal.getPlayerByNameAsync(JSON.stringify(req.params.name), (err, player)=> {
-            if(player.length === 0) return next(new ErrorModel(404, 'player not found'));
-            res.json(player)
+        dal.getPlayerByNameAsync(req.params.name, (err, player)=> {
+            if(player.length === 0) return next(new ErrorModel(404, 'player not found'))
+            if(err) return next(err);
+            res.json(player[0]);
         });
     }
     catch(err){next(err)}
